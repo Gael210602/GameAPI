4fdfe0776a5425bacafe04b2676a935798d80bf6
Gael Perez <gael.perez@bestowinc.com>
2022-04-21 14:41:46 -0500

    Initial commit

---
+++ myapi/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('users/', views.user_list),
+    path('sessions/', views.session_list),
+    path('user/<int:pk>/', views.user_detail),
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('levels/', views.levels_list),
+    path('level/<int:pk>/', views.level_detail),
+    path('attempts/', views.attempt_list),
+    path('attempt/<int:pk>', views.levels_list),
+
+]
---
+++ myapi/views.py
@@ -0,0 +1,48 @@
+from django.views.decorators.csrf import csrf_exempt
+from gameapi.constants import not_allowed
+from myapi.methods.level_detail_Actions import single_level_actions
+from myapi.methods.levels_list import level_actions
+from myapi.methods.login import loginmethod
+from myapi.methods.logout import logout_method
+from myapi.methods.session_list import session_list_actions
+from myapi.methods.single_user_actions import single_user_actions
+from myapi.methods.userActions import user_actions
+
+@csrf_exempt
+def user_list(request):
+    return user_actions(request=request)
+
+@csrf_exempt
+def user_detail(request, pk):
+    return single_user_actions(request, pk)
+
+@csrf_exempt
+def login(request):
+    return loginmethod(request=request)
+
+@csrf_exempt
+def logout(request):
+    return logout_method(request=request)
+
+@csrf_exempt
+def session_list(request):
+    return session_list_actions(request=request)
+
+
+@csrf_exempt
+def levels_list(request):
+    return level_actions(request=request)
+
+
+@csrf_exempt
+def level_detail(request, pk):
+    return single_level_actions(request, pk)
+
+@csrf_exempt
+def attempt_list(request):
+    return attempt_list(request)
+
+
+@csrf_exempt
+def attempt_detail(request, pk):
+    return attempt_list(request, pk)
